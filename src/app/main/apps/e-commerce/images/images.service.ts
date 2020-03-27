d784356bf675a02aea250c3476129143298a7bd7
maruthi <maruthi@mtouchlabs.com>
2020-03-27 16:14:12 +0530

    master

--- src/app/main/apps/e-commerce/images/images.service.ts
+++ src/app/main/apps/e-commerce/images/images.service.ts
@@ -7,12 +7,10 @@ import {
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { BehaviorSubject, Observable } from "rxjs";
 import { baseenvironment } from "../../../../../config";
-
 const headers = new HttpHeaders({
   "Content-Type": "application/json",
   "X-Api-Key":baseenvironment.xapikey
 });
-
 @Injectable()
 export class EcommerceBikeBrandService implements Resolve<any> {
   products: any[];
